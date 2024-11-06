aebdd26b1a8f2bcb9f620e026112b60318e04a10
tranhuuvinh1109@gmail.com <tranhuuvinh1109@gmail.com>
2024-11-07 00:00:54 +0700

    main: svg component

---
+++ src/app/assets/svg/SvgTech.tsx
@@ -0,0 +1,162 @@
+import { SVGProps } from "react";
+
+const SvgTech = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="68"
+    height="68"
+    viewBox="0 0 68 68"
+    fill="none"
+    {...props}
+  >
+    <path
+      d="M24.1495 4.92755C25.3871 2.58913 27.8449 0.995783 30.6749 0.995783H54.8486C58.9234 0.995783 62.2266 4.29909 62.2266 8.37376V26.5693C62.2266 30.6441 58.9234 33.9473 54.8486 33.9473H30.6749C26.6001 33.9473 23.2969 30.6441 23.2969 26.5693V9.51953"
+      strokeWidth="1.99218"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <path
+      d="M55.7798 20.1621H29.7297C28.5414 20.1621 27.5781 19.199 27.5781 18.0107V8.39735C27.5781 7.20908 28.5414 6.24579 29.7297 6.24579H55.7798C56.9681 6.24579 57.9314 7.20908 57.9314 8.39735V18.0107C57.9314 19.199 56.9681 20.1621 55.7798 20.1621Z"
+      strokeWidth="1.99218"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <path
+      d="M17.0901 13.4997C17.0901 11.3485 14.2419 9.6047 10.7286 9.6047C7.21522 9.6047 4.36719 11.3485 4.36719 13.4997C4.36719 15.6507 5.83091 17.3945 10.7286 17.3945C15.6264 17.3945 17.0901 15.6507 17.0901 13.4997Z"
+      strokeWidth="1.99218"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <path
+      d="M23.2931 21.9862H6.1037C5.04877 21.9862 4.12718 22.6993 3.86235 23.7204L3.46976 25.2346C3.22818 26.1658 3.93115 27.0742 4.89298 27.0742H23.3123"
+      strokeWidth="1.99218"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <path
+      d="M16.9527 12.6991C16.9618 12.574 16.968 12.4463 16.968 12.3141C16.968 8.68497 13.8712 2.39486 10.7262 2.39486C7.58103 2.39486 4.48438 8.68497 4.48438 12.3141C4.48438 12.4465 4.49048 12.5741 4.49965 12.6992"
+      strokeWidth="1.99218"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <path
+      d="M8.10156 27.0755V37.0072C8.10156 42.8263 12.8358 47.5605 18.6551 47.5605H26.6537C32.4729 47.5605 37.2073 42.8263 37.2073 37.0072"
+      strokeWidth="1.99218"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <path
+      d="M31.9562 33.9472V37.0076C31.9562 39.9316 29.5773 42.3105 26.6533 42.3105H18.6546C15.7305 42.3105 13.3516 39.9316 13.3516 37.0076V27.0759"
+      strokeWidth="1.99218"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <path
+      d="M13.3516 21.9863V17.1792"
+      strokeWidth="1.99218"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <path
+      d="M8.10156 17.1792V21.9863"
+      strokeWidth="1.99218"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <path
+      d="M48.3125 53.9629V33.9468"
+      strokeWidth="1.99218"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <path
+      d="M37.2109 33.9468V53.9629"
+      strokeWidth="1.99218"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <path
+      d="M60.1484 54.2367C60.6052 54.5367 60.9068 55.0535 60.9068 55.6409V58.1649C60.9068 59.0922 60.1552 59.8438 59.228 59.8438H26.2883C25.3611 59.8438 24.6094 59.0922 24.6094 58.1649V55.6409C24.6094 54.7137 25.3611 53.9622 26.2883 53.9622H55.4665"
+      strokeWidth="1.99218"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <path
+      d="M24.778 58.8958C22.6382 59.0079 20.9375 60.7782 20.9375 62.9459C20.9375 65.186 22.7534 67.002 24.9936 67.002C27.2336 67.002 29.0496 65.186 29.0496 62.9459C29.0496 61.7009 28.4882 60.5874 27.6051 59.8435"
+      strokeWidth="1.99218"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <path
+      d="M60.7403 58.8958C62.8801 59.0079 64.5808 60.7782 64.5808 62.9459C64.5808 65.186 62.7649 67.002 60.5247 67.002C58.2847 67.002 56.4688 65.186 56.4688 62.9459C56.4688 61.7009 57.0303 60.5874 57.9132 59.8435"
+      strokeWidth="1.99218"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <path
+      d="M51.6094 26.5465C51.6094 25.0664 52.8093 23.8665 54.2894 23.8665C55.7696 23.8665 56.9694 25.0664 56.9694 26.5465C56.9694 28.0267 55.7696 29.2266 54.2894 29.2266C52.8093 29.2266 51.6094 28.0267 51.6094 26.5465Z"
+      strokeWidth="1.99218"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <path
+      d="M35.3122 26.5469H31.0703"
+      strokeWidth="1.99218"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <path
+      d="M45.2653 26.5469H41.0234H45.2653Z"
+      strokeWidth="1.99218"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <path
+      d="M22.6562 42.3101V47.5605"
+      strokeWidth="1.99218"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <path
+      d="M8.10156 33.1016H13.3522"
+      strokeWidth="1.99218"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <path
+      d="M11.1953 44.4648L14.9062 40.7539"
+      strokeWidth="1.99218"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <path
+      d="M34.1172 44.4648L30.4062 40.7539"
+      strokeWidth="1.99218"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+  </svg>
+);
+
+export default SvgTech;
